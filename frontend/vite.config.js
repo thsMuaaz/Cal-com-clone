7705a8f41314f3a5fce7a60ced2d5d2d62069c61
thsMuaaz <muaazcdg@gmail.com>
2026-03-29 13:26:34 +0530

    update api url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://cal-com-clone-1.onrender.com',
         changeOrigin: true,
       },
     },
